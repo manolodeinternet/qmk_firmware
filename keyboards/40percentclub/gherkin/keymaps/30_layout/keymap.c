a4807037e9ee9cbd8e3af0b72fd1fd7460e257df
Navarro <manolodeinternet@gmail.com>
2019-02-03 10:50:52 +0100

    [#002] added files: * QMK_issues_log.txt          // by grabbing all the problems that I've been finding along the way * sublime_text_shortcuts.txt  // for grabbing all Sublime Text useful shortcuts enlarge code: * enlarged keymap.c file with the following content:     * layers defined,     * readability codes defined,     * backlightning layers levels defined,     * backlightning situations defined,     * backlight breathing levels defined,     * extended (XTND) commands defined -for OSX windows management

--- keyboards/40percentclub/gherkin/keymaps/30_layout/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/30_layout/keymap.c
@@ -49,3 +49,174 @@
 // my_own_preprocessing_code_keywords
 
 #include QMK_KEYBOARD_H
+
+// DEFINING LAYERS
+#define GHKN   0  // gherkin DEFAULT         layer  
+#define NMBR   1  // numbers                 layer
+#define SYMB   2  // symbols                 layer
+
+#define FNCT   3  // functions & extended f. layer
+#define L_XTND 4  // LEFT  extended default  layer
+#define R_XTND 5  // RIGHT extended default  layer
+#define APPS   6  // apps                    layer
+
+#define SUSR   7  // SUPER USER productivity layer
+
+#define PVIM   8  // Personal            VIM layer
+#define DVIM   9  // Delete              VIM layer
+#define SVIM  10  // Select              VIM layer
+#define XVIM  11  // eXtra               VIM layer
+#define ZVIM  12  // Z extra             VIM layer
+#define AVIM  13  // Alignment           VIM layer
+
+#define MOUS  14  // mouse                   layer
+
+#define BLIT  15  // backlight               layer
+// defining layers
+
+// DEFINING READABILITY CODES
+#define _______ KC_TRNS
+#define NO_KEY  KC_NO
+#define XXXXXXX KC_NO
+// defining readability codes
+
+// DEFINING LAYER LEVELS OF BACKLIGHTNING
+#define BL_OFF  0
+#define BL_MIN  1
+#define BL_LOW  3
+#define BL_MED  7
+#define BL_HGH 12
+#define BL_MAX 15
+
+#define BL_GHKN  0
+#define BL_NMBR  8
+#define BL_SYMB  8
+#define BL_FNCT  5
+#define BL_XTND  5
+#define BL_APPS 10
+
+#define BL_PVIM  8
+#define BL_DVIM 15
+#define BL_SVIM 10
+#define BL_VIM   6
+
+#define BL_MOUS  8
+#define BL_SUSR 15
+// defining layer levels of backlightning
+
+// DEFINING BACKLIGHTNING SITUATIONS
+#define BL_CAPS 15
+#define BL_RESE 15
+// defining backlightning situations
+
+// DEFINING BACKLIGHT BREATHING LELVELS
+#define BR_CAPS  1
+#define BR_NMBR  3
+#define BR_DFLT  6
+// defining backlight breathing lelvels
+
+// DEFINING XTND COMMANDS
+#define PREV_APP       LSFT(LGUI(KC_TAB))  // previous app
+#define NEXT_APP             LGUI(KC_TAB)  //     next app
+#define PREV_WIN     LSFT(LGUI(KC_GRAVE))  // previous window
+#define NEXT_WIN           LGUI(KC_GRAVE)  //     next window
+#define PREV_TAB       LSFT(LCTL(KC_TAB))  // previous tab
+#define NEXT_TAB             LCTL(KC_TAB)  //     next tab
+
+#define SPT_SIRI             LGUI(KC_SPC)  // spotlight & siri
+
+#define     DESK                    KC_F5
+#define APP_WIND            LCTL(KC_DOWN)  // applications windows
+#define MISS_CTL              LCTL(KC_UP)  // mission control
+#define    SPEAK   LCTL(LALT(LGUI(KC_H)))
+#define  LNCHPAD                   KC_F17  // F17 (set at 'System Preferences/Keyboard/Shortcuts/Launchpad&Doc')
+#define  DASHBRD                   KC_F18
+#define ACTV_WIN              LCTL(KC_F4)  //   active window
+#define FLOA_WIN              LCTL(KC_F6)  // floating window
+
+#define REOPEN_L         LSFT(LGUI(KC_T))  // reopen last closed window/tab
+#define  CLOSE_A               LGUI(KC_Q)  // close application
+#define  CLOSE_W               LGUI(KC_W)  // close window
+#define  CLOSE_T               LGUI(KC_W)  // close tab
+
+#define UNDO                   LGUI(KC_Z)   //UNDO
+#define REDO             LSFT(LGUI(KC_Z))   //REDO
+#define CUT                    LGUI(KC_X)   //CUT
+#define COPY                   LGUI(KC_C)   //COPY
+#define PASTE                  LGUI(KC_V)   //PASTE
+#define P_MATCH    LSFT(LALT(LGUI(KC_V)))   //PASTE & MATCH STYLE
+#define P_ST_HY          LALT(LGUI(KC_V))   //(SUBLIME TEXT) PASTE FROM HISTORY
+// [DELETEME] Already implemented as tap dance
+// defining xtnd commands
+
+// DEFINING SUSR COMMANDS
+#define LCK_SCR          LCTL(LGUI(KC_Q))   //LOCK SCREEN (ask for pasword screen)
+#define LOGOUT     LSFT(LALT(LGUI(KC_Q)))   //logout withow menu popup
+// defining susr commands
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
